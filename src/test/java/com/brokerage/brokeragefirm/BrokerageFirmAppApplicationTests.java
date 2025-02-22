f14145155c08590a41b6f5fe7e10530aaf65926f
Berkay Tutal <berkaytutal@gmail.com>
2025-02-22 16:49:25 +0300

    Initial commit

---
+++ src/test/java/com/brokerage/brokeragefirm/BrokerageFirmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brokerage.brokeragefirm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerageFirmAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
